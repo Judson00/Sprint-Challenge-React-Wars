095bc52cc49a47b9d6f0ed85f38866ee968d4953
Robert J. Mitchell <robert.judson@outlook.com>
2019-12-13 11:54:44 -0500

    got stuff to show up

--- starwars/src/components/Cards.js
+++ starwars/src/components/Cards.js
@@ -1,1 +1,15 @@
 import React from "react"
+
+const Cards = props => {
+  console.log(props);
+  return(
+    <div className='main-card'>
+      <div className='card-container'>
+        <h1 className='name'>Name: {props.name}</h1>
+        <h2>Birthday: {props.birth_year}</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Cards;
