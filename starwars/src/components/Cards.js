69b6418251e42432335edcf68a93ce590dae88c6
Robert J. Mitchell <robert.judson@outlook.com>
2019-12-13 11:44:30 -0500

    axios call, sideEffect completed

--- starwars/src/components/Cards.js
+++ starwars/src/components/Cards.js
@@ -1,13 +1,1 @@
 import React from "react"
-
-function Card(props){
-  const { name } = props;
-
-  return(
-    <div className="cardContainer">
-      <h2>{name}</h2>
-    </div>
-  )
-}
-
-export default Card;
